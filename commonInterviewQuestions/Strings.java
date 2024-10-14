a92ebae4800bc220699e517c129bf9958dc01200
SaishWadkar <saish.wadkar27@gmail.com>
2024-10-14 15:49:19 +0530

    string question in interview

---
+++ commonInterviewQuestions/Strings.java
@@ -0,0 +1,21 @@
+package commonInterviewQuestions;
+
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Stack;
+
+
+
+public class Strings {
+
+    public static void main(String[] args){
+
+        String s1 = new String("Saish");
+        String s2 = "Saish";
+
+        System.out.println(s1.equals(s2)); // true
+        System.out.println(s1 == s2); // false
+    }
+
+}
